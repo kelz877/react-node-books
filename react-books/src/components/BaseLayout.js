5a3300876559de2c2d9b8d0d07343be722f0c098
kelly benson <kellyjbenson@gmail.com>
2019-09-05 11:33:41 -0500

    Added components folder and react-router

---
+++ react-books/src/components/BaseLayout.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import App from './App';
+import {NavLink} from 'react-router-dom';
+
+export class Menu extends Component {
+    render() {
+        return <ul>
+                <li>Home</li>
+                <li>View Books</li>
+                <li>Add Book</li>
+                <li>Update Book</li>
+            </ul>
+    }
+}
+
+export class Footer extends Component {
+    render() {
+        return <div>Footer</div>
+    }
+}
+
+
+export class BaseLayout extends Component {
+    render() {
+        return <div>
+                <Menu />
+                {/* {this.props.children} */}
+                <Footer />
+
+        </div>
+    }
+}
