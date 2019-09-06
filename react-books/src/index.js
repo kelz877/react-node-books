9c579c4748dcd29a378922256423becc582299ce
Umreen <uidigitalcrafts@gmail.com>
2019-09-06 08:52:43 -0500

    changed routes

--- react-books/src/index.js
+++ react-books/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import '../src/css/index.css';
-import '../src/css/App.css';
-=======
-import './index.css';
 import App from './components/App';
->>>>>>> master
 import * as serviceWorker from './serviceWorker';
-import App from './components/App';
+// import App from './components/App';
 import {BaseLayout} from './components/BaseLayout'
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import {DisplayBooks} from '../src/components/DisplayBooks';
